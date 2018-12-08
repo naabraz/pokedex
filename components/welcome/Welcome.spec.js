24b4cc506a80baf2b664633fa49edd597d1219d4
Natalia Braz <naabraz@gmail.com>
2018-12-07 23:07:37 -0200

    Adding test files and config

---
+++ components/welcome/Welcome.spec.js
@@ -0,0 +1,11 @@
+import Welcome from './Welcome'
+
+describe('Welcome component test', () => {
+  it('Should render component', () => {
+    // const wrapper = mount(
+    //   <Welcome />
+    // )
+    // expect(wrapper.find(Welcome)).toExist()
+    expect(2+2).toBe(4)
+  })
+})
