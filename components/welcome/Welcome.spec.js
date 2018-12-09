34fdd34df475546f8809eef0e111c2fab8f829c3
Natalia Braz <naabraz@gmail.com>
2018-12-09 18:21:35 -0200

    Adding snapshot to test component

--- components/welcome/Welcome.spec.js
+++ components/welcome/Welcome.spec.js
@@ -1,11 +1,11 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
 import Welcome from './Welcome'
 
 describe('Welcome component test', () => {
-  it('Should render component', () => {
-    // const wrapper = mount(
-    //   <Welcome />
-    // )
-    // expect(wrapper.find(Welcome)).toExist()
-    expect(2+2).toBe(4)
+  it('Should render component correctly', () => {
+    const welcome = renderer.create(<Welcome />).toJSON()
+    expect(welcome).toMatchSnapshot()
   })
 })
