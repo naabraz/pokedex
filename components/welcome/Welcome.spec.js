c025dfebc9196c7ba24ea22addd0d45c0525d433
Natalia Braz <naabraz@gmail.com>
2018-12-10 11:41:23 -0200

    Inserting enzyme

--- components/welcome/Welcome.spec.js
+++ components/welcome/Welcome.spec.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
+import { shallow } from 'enzyme'
+
 import Welcome from './Welcome'
 
 describe('Welcome component test', () => {
   it('Should render component correctly', () => {
     const welcome = renderer.create(<Welcome />).toJSON()
     expect(welcome).toMatchSnapshot()
+  })
+
+  it('Should shallow', () => {
+    const wrapper = shallow(<Welcome />)
   })
 })
