b42bbf397e509ca4c840b804314c650ec6f53587
Natalia Braz <naabraz@gmail.com>
2019-01-03 17:15:35 -0200

    Adding react navigation configuration

--- src/pages/regions/index.js
+++ src/pages/regions/index.js
@@ -1,1 +1,17 @@
+import React from 'react'
 
+import Wrapper from '../../components/Wrapper'
+import {
+  RegionsList,
+  RegionName,
+} from './style'
+
+const data = [{ key: 'a' }, { key: 'b' }]
+
+const Regions = () => (
+  <Wrapper>
+    <RegionsList data={data} renderItem={({ item }) => <RegionName>{item.key}</RegionName>} />
+  </Wrapper>
+)
+
+export default Regions
---
+++ src/pages/regions/style/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native'
+
+export const RegionsList = styled.FlatList`
+`
+
+export const RegionName = styled.Text`
+`
