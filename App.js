5a803a17423a6fffd9b07dc54a28c75b8478d4f8
Natalia Braz <naabraz@gmail.com>
2019-01-13 19:33:19 -0200

    Removing calling to store
    Removing calling to store when rendering APP

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 
 import Routes from './src/routes'
 
-import reducers from './src/reducers'
-import middleware from './src/middleware'
-
-const store = createStore(reducers, middleware)
-
-const App = () => (
-  <Provider store={store}>
-    <Routes />
-  </Provider>)
+const App = () => (<Routes />)
 
 export default App
